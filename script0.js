d9dd1c6e84095b6f209f1847b12ae466dac6f779
Vyacheslav-ru <75384217+Vyacheslav-ru@users.noreply.github.com>
2021-01-21 17:50:35 +0300

    Update script0.js

--- script0.js
+++ script0.js
@@ -1,16 +1,16 @@
 let name1 = prompt('Как тебя зовут?', 'Землянин');
-let age = prompt('Сколько тебе лет?', 22);
+// let age = prompt('Сколько тебе лет?', 22);
 
 if (name1 === null) {
     name1 = 'Землянин';
 }
-age = Number(age);
-let isBoss = confirm(`Тебя зовут ${name1}? И тебе ${age} года?`);
+// age = Number(age);
+// let isBoss = confirm(`Тебя зовут ${name1}? И тебе ${age} года?`);
 
-if (name1 == "" || Number.isNaN(age) || isBoss == false) {
-alert ("Неправильное имя или возраст! Введите правильные данные!");
-location.reload();
-}
+// if (name1 == "" || Number.isNaN(age) || isBoss == false) {
+// alert ("Неправильное имя или возраст! Введите правильные данные!");
+// location.reload();
+// }
 //____________________________________________________↓_innerHTML_↓_____________________________________________________
 
 let NameVisitor = document.querySelector('.NameVisitor');
